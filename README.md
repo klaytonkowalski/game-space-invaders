af5afd1a977b24a1223f1760a27658d54acff2b3
Klayton Kowalski <klaytonkowalski@gmail.com>
2021-08-27 15:49:38 -0400

    Added preview gif.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Space Invaders
+Space Invaders clone made with Raylib.  
+My second game with Raylib! Took roughly ten hours to complete.
+
+Please click the "Star" button on GitHub if you find this project to be interesting!
+
+![alt text](https://github.com/klaytonkowalski/game-space-invaders/blob/main/Thumbnail.png?raw=true)
+
+## Controls
+This game uses the following controls:
+  - \<A\> Move left
+  - \<D\> Move right
+  - \<M\> Toggle music
+  - \<Enter\> Shoot / Continue
+  - \<Escape\> Exit application
--- SpaceInvaders.c
+++ SpaceInvaders.c
@@ -1,9 +1,49 @@
+//////////////////////////////////////////////////////////////////////
+// LICENSE
+//////////////////////////////////////////////////////////////////////
+
+// MIT License
+
+// Copyright (c) 2021 Klayton Kowalski
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// https://github.com/klaytonkowalski/game-space-invaders
+
+//////////////////////////////////////////////////////////////////////
+// INCLUDES
+//////////////////////////////////////////////////////////////////////
+
 #include "raylib.h"
 #include "raymath.h"
 #include <stdio.h>
 
+//////////////////////////////////////////////////////////////////////
+// DEFINES
+//////////////////////////////////////////////////////////////////////
+
 #define MAX_ALIEN_COUNT 128
 #define MAX_BULLET_COUNT 256
+
+//////////////////////////////////////////////////////////////////////
+// ENUMERATIONS
+//////////////////////////////////////////////////////////////////////
 
 typedef enum GameState
 {
@@ -14,6 +54,10 @@ typedef enum GameState
     loseState
 }
 GameState;
+
+//////////////////////////////////////////////////////////////////////
+// STRUCTURES
+//////////////////////////////////////////////////////////////////////
 
 typedef struct Player
 {
@@ -37,6 +81,10 @@ typedef struct Alien
     bool alive;
 }
 Alien;
+
+//////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////
 
 static const int screenWidth = 960;
 static const int screenHalfWidth = 480;
@@ -64,8 +112,22 @@ static const float delayThreshold = 3;
 static const float animationThreshold = 0.5;
 static const int animationFrameCount = 2;
 
+//////////////////////////////////////////////////////////////////////
+// LOADED PROPERTIES
+//////////////////////////////////////////////////////////////////////
+
 static Texture2D playerTexture;
 static Texture2D alienTexture;
+
+static Sound shootSound;
+static Sound playerDeathSound;
+static Sound alienDeathSound;
+
+static Music music;
+
+//////////////////////////////////////////////////////////////////////
+// PROPERTIES
+//////////////////////////////////////////////////////////////////////
 
 static GameState gameState;
 static Player player;
@@ -84,10 +146,10 @@ static int alienFrameIndex;
 static float alienFrameElapsed;
 static bool alienDirection;
 static int alienCount;
-static Sound shootSound;
-static Sound playerDeathSound;
-static Sound alienDeathSound;
-static Music music;
+
+//////////////////////////////////////////////////////////////////////
+// FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////
 
 void Initialize();
 void Update();
@@ -123,6 +185,10 @@ void UpdateAlienAnimations();
 
 void ShootPlayerBullet();
 void ShootAlienBullet(int alienIndex);
+
+//////////////////////////////////////////////////////////////////////
+// FUNCTIONS
+//////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[])
 {
@@ -192,6 +258,8 @@ void Update()
     else if (gameState == loseState)
         UpdateLoseState();
     UpdateMusicStream(music);
+    if (IsKeyPressed(KEY_M))
+        IsMusicPlaying(music) ? PauseMusicStream(music) : ResumeMusicStream(music);
 }
 
 void Draw()
@@ -291,6 +359,10 @@ void FromLoseToStartState()
     loseElapsed = 0;
     player.position = (Vector2) { screenHalfWidth - playerHalfWidth, screenHalfHeight - playerHalfHeight };
     player.livesRemaining = 3;
+    for (int i = 0; i < MAX_ALIEN_COUNT; ++i)
+    {
+        aliens[i].alive = false;
+    }
     alienCount = 0;
     wave = 1;
 }
@@ -310,7 +382,7 @@ void DrawReadyState()
     sprintf(readyBuffer, "Ready Wave %d!", wave);
     const int readyHalfWidth = MeasureText(readyBuffer, textSize) * 0.5;
     DrawText(readyBuffer, screenHalfWidth - readyHalfWidth, 40, textSize, WHITE);
-    if (wave == 25)
+    if (wave >= 19)
     {
         const int maxHalfWidth = MeasureText("Maximum Difficulty", textSize) * 0.5;
         DrawText("Maximum Difficulty", screenHalfWidth - maxHalfWidth, 80, textSize, RED);
@@ -461,7 +533,7 @@ void UpdatePlayState()
                     newAlienDirection = 0;
                 }
             }
-            if (GetRandomValue(1, Clamp(300 - (wave - 1) * 10, 60, 300)) == 1)
+            if (GetRandomValue(1, Clamp(300 - (wave - 1) * 10, 120, 300)) == 1)
             {
                 ShootAlienBullet(i);
             }
